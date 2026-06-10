e7a18ade6e593189b4dfa3cbd79b9d4d699c8bfa
Prathamesh S. Dahake <pratham.dahakepsd@gmail.com>
2026-06-10 16:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    // Call the setters using the arguments
+    this.name = name;
+    this.age = age;
+  }
 
-class Student extends Person {}
+  // Use a backing variable (_name) to store the data
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  set name(value) {
+    this._name = value;
+  }
+
+  // Use a backing variable (_age) to store the data
+  get age() {
+    return this._age;
+  }
+
+  set age(val) {
+    // Setters must NOT return a value; they just assign it
+    this._age = val;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
